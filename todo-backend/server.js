0f6762d11f981b49936d658d900f6cf23c0d8574
Senal Perera <senalperera777@gmail.com>
2026-03-01 21:17:54 +0530

    Implemented code to connect MongoDB

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 const app = express()
 app.use(cors()) // enabling CORS for all routes
+ 
+// Connecting to mongo
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))
 
+// Routes defined
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
