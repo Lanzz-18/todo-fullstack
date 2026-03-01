8b846ab1433829b9fb430bb643a5c2766f36b3ef
Senal Perera <senalperera777@gmail.com>
2026-03-01 21:00:59 +0530

    Initialized frontend files
    - Added basic todo functions

--- todo-frontend/todo-frontend/src/App.jsx
+++ todo-frontend/todo-frontend/src/App.jsx
@@ -1,33 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos")
+    return saved ? JSON.parse(saved) : []
+  })
+  const [input, setInput] = useState("")
+  const [options, setOptions] = useState([])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },  [todos]) // whenever todos list change, save it to localstorage
+  
+  const addTodo =()=>{
+    if(input=="") return // refuse added empty tasks
+    setTodos([...todos, { id: Date.now(), text: input, completed: false }])
+    setInput("") // clear input
+  }
+
+  const deleteTodo = (id) =>{
+    // Filtering out the task to be deleted and setting it out
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(todos.map(todo => {
+      todo.id === id ? {...todo, completed: !todo.completed } : todo // if id doesn't match, just return the original task
+    }))
+  }
+
+  const addOption = () => {
+      if(input=="") return
+      setOptions([...options, {id: Date.now(), text: input}])
+      setInput("")
+  }
 
   return (
+    // Add drop down for type
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="search-group">
+        <input
+          type="text"
+          placeholder="Add task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <select id="select-group">
+          <option>option 1</option>
+        </select>
+        <button onClick={addTodo}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <input
+        type="text"
+        placeholder="Add a category"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addOption}>Add category</button>
+
+      <div className="todo-list">
+        {todos.map(todo => (
+          <div key={todo.id} className="todo-item">
+            <p 
+              style={{textDecoration: todo.completed ? "line-through" : "none"}}
+              onClick={() => toggleComplete(todo.id)}>
+              {todo.text}
+            </p>
+            <button onClick={() => deleteTodo(todo.id)}>🗑️</button>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
