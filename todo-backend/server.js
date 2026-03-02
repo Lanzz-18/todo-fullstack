221462298a56c5e7ac69a861fe7ae179f7a6024f
Senal Perera <senalperera777@gmail.com>
2026-03-02 14:58:30 +0530

    Update server.js
    added todo and auth routes to server + bug fixes

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -3,8 +3,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+const authRoutes = require('./routes/auth')
+const todoRoutes = require('./routes/todos')
+
 const app = express()
 app.use(cors()) // enabling CORS for all routes
+app.use(express.json())
  
 // Connecting to mongo
 mongoose.connect(process.env.MONGO_URI)
@@ -15,6 +19,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
+app.use('/api/auth', authRoutes)
+app.use('/api/todos/', todoRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
