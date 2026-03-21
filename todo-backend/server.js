5b3fd3fd5849c4ee09493b4b1e1a3317fa125cf4
Senal Perera <senalperera777@gmail.com>
2026-03-21 09:57:43 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config()
+
 
 const authRoutes = require('./routes/auth')
 const todoRoutes = require('./routes/todos')
