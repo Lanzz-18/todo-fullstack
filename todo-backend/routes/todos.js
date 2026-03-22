a529abede714d1328068d402218626c27beefc12
Senal Perera <senalperera777@gmail.com>
2026-03-22 18:27:55 +0530

    refactor: add middleware protect for all the routes

--- todo-backend/routes/todos.js
+++ todo-backend/routes/todos.js
@@ -1,50 +1,54 @@
 const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
-const jwt = require("jsonwebtoken");
-
-// Middleware to verify token
-const auth = (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1]
-  if (!token){
-    return res.status({
-      message: 'No token'
-    })
-  }
-  try{
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = decoded
-    next()
-  } catch {
-    res.status(401).json({ message: 'Invalid token'})
-  }
-}
+const { protect } = require("../middleware/auth");
 
 // Get all todos from a user
-router.get("/", auth, async (req, res) => {
-  const todos = await Todo.find({ user: req.user.id });
-  res.json(todos);
-});
+router.get("/", protect, async (req, res, next) => {
+  try {
+    const todos = await Todo.find({ user: req.user.id });
+    res.json(todos);
+  } catch (err) {
+    next(err);
+  }
+ });
 
 // Add todo
-router.post("/", auth, async (req, res) => {
-  const todo = new Todo({ ...req.body, user: req.user.id });
-  await todo.save();
-  res.json(todo);
+router.post("/", protect, async (req, res, next) => {
+  try {
+    const todo = new Todo({ ...req.body, user: req.user.id });
+    await todo.save();
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Delete todo
-router.delete("/:id", auth, async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: "Todo deleted" });
+router.delete("/:id", protect, async (req, res, next) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({ message: "Todo deleted" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Mark as complete
-router.patch("/:id", auth, async(req, res) => {
-  const todo = await Todo.findById(req.params.id)
-  todo.completed = !todo.completed
-  await todo.save()
-  res.json(todo)
+router.patch("/:id", protect, async(req, res, next) => {
+  try {
+    const todo = await Todo.findById(req.params.id)
+    if (!todo) {
+      return res.status(404).json({
+        message: "Todo not found"
+      });
+    }
+    todo.completed = !todo.completed
+    await todo.save()
+    res.status(200).json(todo)
+  } catch (err) {
+    next(err);
+  }
 })
 
 module.exports = router;
